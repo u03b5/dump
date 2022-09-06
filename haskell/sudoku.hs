bfd66b544c99a98577dc75fb4412f4c166ac63ea
u03b5 <111270909+u03b5@users.noreply.github.com>
2022-09-06 14:42:02 -0700

    more

---
+++ haskell/sudoku.hs
@@ -0,0 +1,25 @@
+module Main where
+import Data.List
+
+type Grid = Matrix Valve
+type Matrix a = [Row a]
+type Row a = [a]
+type Valve = Char
+
+empty :: Grid
+empty = replicate 9 (replicate 9 '.')
+
+-- identity function for rows, nice utility to have
+rows :: Matrix a -> [Row a]
+rows = id
+
+-- id = cols . cols
+cols :: Matrix a -> [Row a]
+cols = transpose
+
+boxes :: Matrix a -> [Row a]
+boxes = undefined
+
+main :: IO ()
+main = undefined
+
